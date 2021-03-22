af2ab31a99f142f301932c9f6c488230ecdd410a
gisung2215 <wjv2215@naver.com>
2021-03-22 21:50:05 +0900

    [frontend] feat : 내가 쓴 리뷰 추가정보

--- frontend/src/components/static/mypage/MyReviews.vue
+++ frontend/src/components/static/mypage/MyReviews.vue
@@ -2,16 +2,32 @@
   <div class="reviews-fram">
     <div class="reviews-item">
       <div class="reviews-Img">
-        <img class="wineImg" :src="review.img" alt="" />
+        <img class="wineImg" src="@/assets/images/wine02.png" alt="" />
       </div>
     </div>
     <div class="reviews-item">
-      <div class="wine-title">{{ review.reviewTitle }}</div>
+      <div class="wine-title">{{ review.wineTitle }}</div>
+      <div class="review-title">
+        <span>{{ review.reviewTitle }}</span>
+        <img src="@/assets/images/star-rating.png" alt="" />
+        <img src="@/assets/images/money-icons.png" alt="" />
+      </div>
+      <div class="review-writer" style="background-color: pink">
+        <div class="writer-img">
+          <img :src="userInfo.profile" alt="" />
+        </div>
+        <div class="addition-info">
+          <span class="addition-info-usernickname">{{
+            userInfo.nickname
+          }}</span>
+          <span class="addition-info-date">{{ review.date }}</span>
+        </div>
+      </div>
     </div>
     <div class="reviews-item">
       <div class="reviews-btn">
-        <button>와인으로 이동하기</button>
-        <button>리뷰로 이동하기</button>
+        <a href="#" class="button">와인으로 이동하기</a>
+        <a href="#" class="button">리뷰로 이동하기</a>
       </div>
     </div>
   </div>
@@ -19,49 +35,106 @@
 
 <script>
 export default {
-  props: ['review'],
+  props: ["review", "userInfo"],
 };
 </script>
 
 <style scoped>
 .reviews-fram {
-  width: 98%;
+  width: 90%;
   height: 150px;
   display: flex;
   flex-direction: row;
-  flex-wrap: nowrap;
   margin: 15px;
+  border-radius: 2em;
+  /* overflow: hidden; */
 }
 .reviews-item:nth-child(1) {
-  flex: 1 1 0;
-  background-color: #91091e;
+  width: 150px;
 }
 .reviews-item:nth-child(2) {
   flex: 2 1 0;
-  background-color: #da723c;
+  display: flex;
+  flex-direction: column;
+  background-color: #f4f4f4;
+  padding-left: 5px;
 }
 .reviews-item:nth-child(3) {
-  flex: 1 1 0;
-  background-color: #c39e5c;
+  width: 250px;
+  background-color: #f4f4f4;
   display: flex;
   align-items: center;
 }
 .reviews-Img {
+  position: relative;
+
   width: 150px;
-  height: 150px;
-  border-radius: 70%;
-  overflow: hidden;
+  height: 170px;
+  z-index: 5;
 }
 
 .wineImg {
+  position: absolute;
+  top: -15px;
+  left: 10px;
   width: 100%;
   height: 100%;
   object-fit: cover;
 }
+
+/* ###########  리뷰정보 중간지점!!  ################# */
+/* ################################################# */
 .wine-title {
   font-size: 32px;
   font-weight: bold;
 }
+.review-title {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 8px;
+}
+
+.review-title > span {
+  font-size: 16px;
+}
+.review-title > img {
+  margin-left: 8px;
+}
+
+.review-writer {
+  position: relative;
+  top: 50px;
+  display: flex;
+}
+
+.writer-img {
+  width: 35px;
+  height: 35px;
+  border-radius: 70%;
+  overflow: hidden;
+}
+
+.writer-img > img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.addition-info {
+  margin-left: 5px;
+  display: flex;
+  flex-direction: column;
+}
+.addition-info-usernickname {
+  font-size: 20px;
+  font-weight: 550;
+  color: var(--basic-color-bg2);
+}
+.addition-info-date {
+  color: #808080;
+}
+/* ###########  리뷰정보 마지막 지점!!  ################# */
+/* ##################################################### */
 .reviews-btn {
   width: 100%;
   display: flex;
@@ -69,8 +142,7 @@ export default {
   justify-content: center;
 }
 
-.reviews-btn > button {
-  background-color: #7c7878;
+.reviews-btn > a {
   border: none;
   padding: 15px 0;
   text-align: center;
@@ -79,5 +151,15 @@ export default {
   font-size: 15px;
   margin: 4px;
   cursor: pointer;
+}
+a.button {
+  color: black;
+  font-weight: 600;
+  box-shadow: rgba(250, 250, 250, 1) 0 0px 0px 0px inset;
+}
+
+a.button:hover {
+  color: rgba(238, 236, 243, 1);
+  box-shadow: rgba(53, 51, 59, 0.7) 0 0px 0px 40px inset;
 }
 </style>
--- frontend/src/views/Mypage.vue
+++ frontend/src/views/Mypage.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- fram -> container -> wrap -->
   <div class="fram">
-    <div class="item" style="background-color: #e84545">
+    <div class="item" style="background-color: #242323">
       <div class="left-wrap">
         <span class="nav-title">Mypage</span>
         <div class="nav-btn">
@@ -12,21 +12,24 @@
       </div>
     </div>
 
-    <div class="item" style="background-color: #2b2e4a">
+    <div class="item" style="background-color: #0f0f0f">
       <div class="right-wrap">
         <div class="content">
           <div class="imgBox">
-            <img class="profile" :src="this.userInfo.profile" alt="프로필 이미지" />
+            <img
+              class="profile"
+              :src="this.userInfo.profile"
+              alt="프로필 이미지"
+            />
           </div>
           <span id="userId">{{ this.userInfo.nickname }}</span>
           <div class="content-item">
-            <MyReviews v-for="(review, idx) in reviews" :key="idx" :review="review" />
-          </div>
-          <div class="content-item">
-            <MyReviews v-for="(review, idx) in reviews" :key="idx" :review="review" />
-          </div>
-          <div class="content-item">
-            <MyReviews v-for="(review, idx) in reviews" :key="idx" :review="review" />
+            <MyReviews
+              v-for="(review, idx) in reviews"
+              :key="idx"
+              :review="review"
+              :userInfo="userInfo"
+            />
           </div>
         </div>
       </div>
@@ -35,44 +38,55 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
-import MyReviews from '@/components/static/mypage/MyReviews.vue';
+import { mapState, mapMutations } from "vuex";
+import MyReviews from "@/components/static/mypage/MyReviews.vue";
+
+// const aa = "@/assets/images/wine01.png";
 
 export default {
-  name: 'Mypage',
+  name: "Mypage",
   components: {
     MyReviews,
   },
 
   data: () => ({
     cnt: 4,
-    info: '',
+    info: "",
     reviews: [
       {
-        img: 'https://www.lacasabodega.com/wp-content/uploads/2019/02/Botella_Sembro.jpg',
-        reviewTitle: '1번 딸기맛이 나요',
+        img: "https://img.maisonkorea.com/2020/04/msk_5e8d712726056.jpg",
+        wineTitle: "까베르네 소비뇽",
+        reviewTitle: "딸기맛이 나요",
         reviewScore: 4,
+        date: "2021.03.22",
       },
       {
-        img: 'https://img.maisonkorea.com/2020/04/msk_5e8d7129f1d53.jpg',
-        reviewTitle: '2번 바나나맛이 나요',
+        img: "https://img.maisonkorea.com/2020/04/msk_5e8d7129f1d53.jpg",
+        wineTitle: "샤르도네",
+        reviewTitle: "바나나맛이 나요",
         reviewScore: 4,
+        date: "2021.02.18",
       },
       {
-        img: 'http://www.sommeliertimes.com/news/photo/201905/13382_26893_4347.jpg',
-        reviewTitle: '3번 사과맛이 나요',
+        img:
+          "http://www.sommeliertimes.com/news/photo/201905/13382_26893_4347.jpg",
+        wineTitle: "피노 누아",
+        reviewTitle: "사과맛이 나요",
         reviewScore: 4,
+        date: "2021.01.29",
       },
       {
-        img: 'http://www.sommeliertimes.com/news/photo/201905/13382_26893_4347.jpg',
-        reviewTitle: '3번 사과맛이 나요',
-        reviewScore: 4,
+        img:
+          "http://www.sommeliertimes.com/news/photo/201905/13382_26893_4347.jpg",
+        wineTitle: "소비뇽 블랑",
+        reviewTitle: "사과맛이 나요",
+        date: "2020.12.08",
       },
     ],
   }),
   computed: {
-    ...mapState('userInfo', ['userInfo']),
-    ...mapState('nav', ['navActive']),
+    ...mapState("userInfo", ["userInfo"]),
+    ...mapState("nav", ["navActive"]),
   },
 };
 </script>
@@ -113,9 +127,10 @@ export default {
 .nav-title {
   font-size: 40px;
   font-weight: bold;
+  color: #9b9191;
   margin: 5px;
   position: absolute;
-  bottom: 65px;
+  bottom: 75px;
   left: 10px;
   transform: rotate(-90deg);
 }
@@ -125,9 +140,10 @@ export default {
 }
 .nav-btn > button {
   border: 1px solid black;
-  background: #903749;
+  background: black;
   border-radius: 20px;
   font-weight: bold;
+  color: #a3a1a1;
   width: 120px;
   height: 25px;
   left: 80px;
@@ -156,8 +172,10 @@ export default {
   margin-top: 150px;
   width: 90%;
   height: auto;
-  background-color: white;
+  background-color: #ffffff;
   border-radius: 2em;
+  display: flex;
+  justify-content: center;
 }
 .imgBox {
   position: absolute;
@@ -177,15 +195,17 @@ export default {
 #userId {
   font-size: 32px;
   font-weight: bold;
+  color: var(--basic-color-bg2);
   position: absolute;
   top: -40px;
   left: 210px;
 }
 
 .content-item {
-  position: relative;
+  width: 100%;
   margin-top: 100px;
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
 </style>

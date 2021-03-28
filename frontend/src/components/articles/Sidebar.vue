696c5f774cf9f7301c5e5c3910743f150bdebdec
BaeYongryeol <raon0725@naver.com>
2021-03-28 14:33:53 +0900

    [frontend] feat : sidebar component화

---
+++ frontend/src/components/articles/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="sidebar"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#sidebar {
+  width: 172px;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.502);
+}
+</style>

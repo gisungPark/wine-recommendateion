2e46f8a0befe8ce27fc5209092bedac3d5fafed6
박홍균 <phk2246@naver.com>
2021-04-01 10:34:47 +0900

    [backend] feat : 탑10 가져오기

--- backend/yourwine/src/main/java/com/ssafy/yourwine/config/SecurityConfig.java
+++ backend/yourwine/src/main/java/com/ssafy/yourwine/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/reco/getTopten","/reco/food-recolist/**","/wine/**","/user/signup", "/user/checkemail/**", "/user/checknickname/**", "/user/checkuser",
+                .antMatchers("/reco/getTopten/**","/reco/food-recolist/**","/wine/**","/user/signup", "/user/checkemail/**", "/user/checknickname/**", "/user/checkuser",
                             "/user/login", "/user/checkmail")
                 .permitAll()
                 .antMatchers(
